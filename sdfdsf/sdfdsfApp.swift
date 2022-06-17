af0dc1467a136d425eafda97dec059a6108326cb
PapiaMCode <papiamcode@gmail.com>
2022-06-16 18:28:18 -0700

    Initial Commit

---
+++ sdfdsf/sdfdsfApp.swift
@@ -0,0 +1,20 @@
+//
+//  sdfdsfApp.swift
+//  sdfdsf
+//
+//  Created by Jannatun Nahar Papia on 16/6/22.
+//
+
+import SwiftUI
+
+@main
+struct sdfdsfApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
